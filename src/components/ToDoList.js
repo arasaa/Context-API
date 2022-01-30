cc354c743802f4ed6e013f65e25f6c210eff414f
araz saydu <arazsaydu@gmail.com>
2022-01-30 10:07:05 +0100

    added useContext

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,22 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {ThemeContext} from '../contexts/ThemeContext'
 
-class ToDoList extends React.Component {
-    static contextType = ThemeContext;
-
-    render () {
-        const { isDarkTheme, darkTheme, lightTheme, changeTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-        return(
-              
-                <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}} >
-                    <p className="item">plan the family trip</p>
-                    <p className="item">Go for shopping</p>
-                    <p className="item">Go for a walk</p>
-                    <button className='ui button primary' onClick={changeTheme}>Change Theme</button>
-                
-                </div>
-        )
-    }
+const ToDoList = () =>{
+    const { isDarkTheme, darkTheme, lightTheme, changeTheme} = useContext(ThemeContext)
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    return(
+        <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}} >
+        <p className="item">plan the family trip</p>
+        <p className="item">Go for shopping</p>
+        <p className="item">Go for a walk</p>
+        <button className='ui button primary' onClick={changeTheme}>Change Theme</button>
+    
+    </div>
+    )
 }
 export default ToDoList;
